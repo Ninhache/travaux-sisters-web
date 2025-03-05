92ee376059309248723955798e524b4c26ae91f0
Ninhache <mr.carambar27@gmail.com>
2025-03-05 08:59:45 +0100

    fix: improved ux

--- app/(sign-route)/sign-in/page.tsx
+++ app/(sign-route)/sign-in/page.tsx
@@ -1,5 +1,15 @@
 import LoginForm from "@/components/account/login-form";
+import { Metadata } from "next";
 
-export default function Home() {
-  return <LoginForm />;
+export const metadata: Metadata = {
+  title: "Se connecter | Travaux Sisters",
+  description: "Travaux Sisters WIP!",
+};
+
+export default function SignInPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100 p-4">
+      <LoginForm />
+    </main>
+  );
 }
--- app/(sign-route)/sign-up/page.tsx
+++ app/(sign-route)/sign-up/page.tsx
@@ -1,5 +1,15 @@
 import SignUpForm from "@/components/account/sign-up-form";
+import { Metadata } from "next";
 
-export default function Home() {
-  return <SignUpForm />;
+export const metadata: Metadata = {
+  title: "S'enregistrer | Travaux Sisters",
+  description: "Travaux Sisters WIP!",
+};
+
+export default function SignUpPage() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-gradient-to-br from-gray-50 to-gray-100 p-4">
+      <SignUpForm />
+    </main>
+  );
 }
