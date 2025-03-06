d6f720fcf0029b7229149762974480277098d61e
Ninhache <mr.carambar27@gmail.com>
2025-03-06 22:21:36 +0100

    fix: bring back the prodApi

--- config/url.ts
+++ config/url.ts
@@ -1,6 +1,5 @@
 const devAPI = "http://localhost:8080/api";
-// const prodAPI = "https://travaux-sisters.linv.dev/api";
-const prodAPI = "http://localhost:8080/api";
+const prodAPI = "https://travaux-sisters.linv.dev/api";
 
 export function getAPIBaseURL() {
   return process.env.NODE_ENV === "development" ? devAPI : prodAPI;
